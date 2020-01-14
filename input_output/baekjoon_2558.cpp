152ca66d61c3f9dac7130293627bdc89665d77f8
NamNamju <namuju_hi@naver.com>
2020-01-14 21:13:04 +0900

    A+B - 2

---
+++ input_output/baekjoon_2558.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	// cin은 enter와 공백 모두로 받을 수 있다.
+
+	cout << a + b << endl;
+	return 0;
+}
