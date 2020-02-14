01aad5800be9ef87358ec88cdfcb8f7d112158c3
NamNamju <namuju_hi@naver.com>
2020-02-14 19:59:31 +0900

    가장 긴 증가하는 부분 수열

--- DP/baekjoon_11053.cpp
+++ DP/baekjoon_11053.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int main() {
 	int num;
+	int A[1010] = {}; // 수를 입력받는 배열
 	int Dp[1010] = {};
-	int A[1010] = {};
+	// Dp[i]일 경우 A[i] 의 전에 있는 수들 중 A[i]보다 작은 수가 몇 개인지 체크하고, 그것을 Dp[i]에 저장.
+
 	int max = 0;
 	
 	cin >> num;
@@ -16,7 +18,7 @@ int main() {
 
 	for (int i = 1; i <= num; i++)
 	{
-		int min = 0;
+		int min = 0; // A[i] 일 때 작은 수의 개수를 저장.
 		for (int j = 0; j < i; j++) {
 			if (A[i] > A[j]) {
 				if (min < Dp[j])
@@ -24,8 +26,9 @@ int main() {
 			}
 		}
 		Dp[i] = min + 1;
+		cout << Dp[i] << endl;
 		if (max < Dp[i])
-			max = Dp[i];
+			max = Dp[i]; // 작은 수들의 개수가 가장 최대치인 경우를 저장
 	}
 
 	cout << max;
